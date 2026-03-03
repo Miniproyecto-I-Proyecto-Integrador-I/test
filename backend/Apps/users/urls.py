526f76beddf1843ebc90de854cde62db15877b90
Nicolas Cordoba <niccord33@gmail.com>
2026-03-02 20:19:06 -0500

    Agrega al backend autenticacion por JWT, configuracion basica de este y los endpoint de login y register

--- backend/Apps/users/urls.py
+++ backend/Apps/users/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
+from .views import UserViewSet, AuthTokenView
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/login/', AuthTokenView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
